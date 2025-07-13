0e0d5934e67c7c8e810475794658814e126bd626
Hamsini-1223 <159305714+Hamsini-1223@users.noreply.github.com>
2025-07-13 20:34:26 +0530

    Delete dist directory

--- dist/shop-contract.d.ts
+++
@@ -1,9 +0,0 @@
-interface Shop {
-    name: string;
-    isOpen: boolean;
-    open(): void;
-    close(): void;
-    payRent(): boolean;
-}
-export { Shop };
-//# sourceMappingURL=shop-contract.d.ts.map
--- dist/shop-contract.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-// shop-contract.ts
-// This is the basic contract every shop must follow (DIP)
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=shop-contract.js.map
